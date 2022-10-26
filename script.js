87f7d2fcbd97ee3cb3af769b8abe73835ff78743
Karamouche <hugo4ibt@gmail.com>
2022-10-26 17:53:33 +0700

    Add answer when you loose

--- script.js
+++ script.js
@@ -26,19 +26,29 @@ class Game{
 		    "sample": "https://p.scdn.co/mp3-preview/6754a91bc040ffbb2f70980fbcadf86385a109e9?cid=22dd5ffe1bdd4c588e1bf2177b66cc14"
 		}
 
-		var RB = {
-		    "name": "Rêves bizarres (feat. Damso)",
+		var ST = {
+		    "name": "6.3",
 		    "artists": [
-		        "Orelsan",
-		        "Damso"
+		        "Naps",
+		        "Ninho"
 		    ],
-		    "cover": "https://i.scdn.co/image/ab67616d0000b2733331ab0675406c3c6d711c25",
-		    "sample": "https://p.scdn.co/mp3-preview/e712914a1ba6f1bf60e36552311f715d5b992a97?cid=22dd5ffe1bdd4c588e1bf2177b66cc14"
+		    "cover": "https://i.scdn.co/image/ab67616d0000b2732cd6b67be46f4bb11bf5dc4d",
+		    "sample": "https://p.scdn.co/mp3-preview/e61d8897bc22cf65b03e67af2eb15b619974247c?cid=22dd5ffe1bdd4c588e1bf2177b66cc14"
+		};
+
+		var PUFF = {
+		    "name": "PUFF PUFF PUFF",
+		    "artists": [
+		        "Gambi"
+		    ],
+		    "cover": "https://i.scdn.co/image/ab67616d0000b273eb7305df518f55400c11718f",
+		    "sample": "https://p.scdn.co/mp3-preview/0ea7bc87a301be30a9ed2a4810a81bdac668e8b4?cid=22dd5ffe1bdd4c588e1bf2177b66cc14"
 		};
 
 		JCVD = this.jsonToTrack(JCVD);
-		RB = this.jsonToTrack(RB);
-		this.trackList = [JCVD, RB];
+		ST = this.jsonToTrack(ST);
+		PUFF = this.jsonToTrack(PUFF);
+		this.trackList = [JCVD, ST, PUFF];
 		this.inGame = true;
 		this.currentTrack = null;
 		this.audio = null;
@@ -64,7 +74,7 @@ class Game{
 		this.audio.pause();
 		this.audio.currentTime = 0;
 		var input = document.getElementById("input");
-		if (input.value == this.currentTrack.name){
+		if (input.value.toUpperCase() == this.currentTrack.name.toUpperCase()){
 			return true;
 		}
 		return false;
@@ -79,6 +89,19 @@ class Game{
 
 }
 
+function LtoS(L){
+	var result = "";
+	for (element of L){
+		if (element == L[L.length - 1]){
+			result += element;
+		}else if(element == L[L.length - 2]){
+			result += element+" and ";
+		}else{
+			result += element+", ";
+		}
+	}
+	return result;
+}
 
 function validateFormLink(){
 	if (game == null){
@@ -95,14 +118,12 @@ function validateFormLink(){
 		else{
 			game.inGame = false;
 			var text = document.getElementById("main_label");
-			text.textContent = "You lost ! Your score : "+game.score;
+			text.textContent = "You lost ! It was "+ game.currentTrack.name +" by "+ LtoS(game.currentTrack.artists) +"."+'\r\n'+ "Your score : "+game.score;
 			var image = document.getElementById("blindtest-img");
 			image.classList.remove('blur');
-			image["src"] = "https://cdn-icons-png.flaticon.com/512/232/232413.png";
 		}
 	}else{
-		var text = document.getElementById("main_label");
-		text.textContent = "You lost ! Your score : "+game.score;
+		
 	}
 }
 
