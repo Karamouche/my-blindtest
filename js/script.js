0e8473e39885183d77a1cee61b049e0bf23f8346
Karamouche <hugo4ibt@gmail.com>
2022-11-01 17:32:40 +0700

    similarity % check

--- js/script.js
+++ js/script.js
@@ -62,7 +62,11 @@ class Game{
 		//Check if the answer is right
 		this.audio.pause();
 		this.audio.currentTime = 0;
-		if ($("#input").val().toUpperCase() == this.currentTrack.name.toUpperCase()){
+		const similarityAnswer = similarity(
+			$("#input").val().toUpperCase().normalize("NFD").replace(/\p{Diacritic}/gu, ""), 
+			this.currentTrack.name.toUpperCase().normalize("NFD").replace(/\p{Diacritic}/gu, "")
+		);
+		if (similarityAnswer >= 0.6){
 			return true;
 		}
 		return false;
@@ -114,6 +118,51 @@ function LtoS(L){
 	}
 	return result;
 }
+
+
+//FUNCTIONS TO CHECK SIMILARITY
+function editDistance(s1, s2) {
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+
+  var costs = new Array();
+  for (var i = 0; i <= s1.length; i++) {
+    var lastValue = i;
+    for (var j = 0; j <= s2.length; j++) {
+      if (i == 0)
+        costs[j] = j;
+      else {
+        if (j > 0) {
+          var newValue = costs[j - 1];
+          if (s1.charAt(i - 1) != s2.charAt(j - 1))
+            newValue = Math.min(Math.min(newValue, lastValue),
+              costs[j]) + 1;
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
+        }
+      }
+    }
+    if (i > 0)
+      costs[s2.length] = lastValue;
+  }
+  return costs[s2.length];
+}
+
+
+function similarity(s1, s2) {
+  var longer = s1;
+  var shorter = s2;
+  if (s1.length < s2.length) {
+    longer = s2;
+    shorter = s1;
+  }
+  var longerLength = longer.length;
+  if (longerLength == 0) {
+    return 1.0;
+  }
+  return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
 
 function connectSpotify(){
 	const getUrlParameter = (sParam) => {
