8cd70db791052eb62c7f18fe909b2e8359df80f7
Karamouche <hugo4ibt@gmail.com>
2022-11-10 12:41:07 +0700

    updated for mobile

--- js/script.js
+++ js/script.js
@@ -165,23 +165,22 @@ function similarity(s1, s2) {
   return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
 }
 
+const getUrlParameter = (sParam) => {
+	let sPageURL = window.location.search.substring(1),////substring will take everything after the https link and split the #/&
+	sURLVariables = sPageURL != undefined && sPageURL.length > 0 ? sPageURL.split('#') : [],
+	sParameterName,
+	i;
+	let split_str = window.location.href.length > 0 ? window.location.href.split('#') : [];
+	sURLVariables = split_str != undefined && split_str.length > 1 && split_str[1].length > 0 ? split_str[1].split('&') : [];
+	for (i = 0; i < sURLVariables.length; i++) {
+		sParameterName = sURLVariables[i].split('=');
+		if (sParameterName[0] === sParam) {
+		  return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+		}
+	}
+}
 
 function connectSpotify(){
-	const getUrlParameter = (sParam) => {
-		let sPageURL = window.location.search.substring(1),////substring will take everything after the https link and split the #/&
-		sURLVariables = sPageURL != undefined && sPageURL.length > 0 ? sPageURL.split('#') : [],
-		sParameterName,
-		i;
-		let split_str = window.location.href.length > 0 ? window.location.href.split('#') : [];
-		sURLVariables = split_str != undefined && split_str.length > 1 && split_str[1].length > 0 ? split_str[1].split('&') : [];
-		for (i = 0; i < sURLVariables.length; i++) {
-			sParameterName = sURLVariables[i].split('=');
-			if (sParameterName[0] === sParam) {
-			  return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-			}
-		}
-	};
-
     // Get Access Token
     const accessToken = getUrlParameter('access_token');
 	//Application token & redirect uri
@@ -246,3 +245,10 @@ $("input").hide();
 window.ondragstart = function() { return false; } //avoid dragging image
 var game = null;
 var data = null;
+var isToken = getUrlParameter("access_token") != null;
+console.log(isToken);
+if (!isToken){
+	$("#time-frame").hide();
+}else{
+	$("button").text("Start game");
+}
